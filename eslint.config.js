d96ecefb836dbdbac1ac6a716999ccb92f4cf977
frodo33 <tomaszgwozdz95@gmail.com>
2025-12-11 19:08:54 +0100

    feat: eslint tailwind && prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import reactPlugin from 'eslint-plugin-react'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import reactPlugin from "eslint-plugin-react"
 import importPlugin from "eslint-plugin-import"
-import { defineConfig, globalIgnores } from 'eslint/config'
+import tailwindPlugin from "eslint-plugin-tailwindcss"
+import prettierPlugin from "eslint-plugin-prettier"
+import { defineConfig, globalIgnores } from "eslint/config"
+
+import { fileURLToPath } from "url"
+import { dirname, resolve } from "path"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     settings: {
       react: { version: "detect" },
+      tailwindcss: {
+        config: resolve(__dirname, "tailwind.config.js"),
+      },
     },
     plugins: {
-      'import': importPlugin,
+      import: importPlugin,
+      tailwindcss: tailwindPlugin,
+      prettier: prettierPlugin,
     },
     extends: [
       js.configs.recommended,
@@ -23,60 +36,58 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       reactPlugin.configs.flat.recommended,
-      reactPlugin.configs.flat['jsx-runtime'],
+      reactPlugin.configs.flat["jsx-runtime"],
+      tailwindPlugin.configs["flat/recommended"],
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     rules: {
-      'quotes': ['error', 'double'],
-      'no-multiple-empty-lines': ['error', { max: 1, maxEOF: 0, maxBOF: 0, }],
-      'indent': ['error', 2, { "SwitchCase": 1 }],
-      'no-console': 'error',
+      "prettier/prettier": "error",
+      "no-console": "error",
       "object-shorthand": ["error", "always"],
       "arrow-body-style": ["error", "as-needed"],
       "@typescript-eslint/consistent-type-definitions": ["error", "type"],
-      'react/function-component-definition': [
-        'warn',
+      "react/function-component-definition": [
+        "warn",
         {
-          "namedComponents": "arrow-function",
-          "unnamedComponents": "arrow-function"
-        }
+          namedComponents: "arrow-function",
+          unnamedComponents: "arrow-function",
+        },
       ],
       "import/order": [
         "error",
         {
-          "distinctGroup": false,
-          "groups": [
-            "builtin",
-            "external",
-            "internal",
-            "index"
-          ],
-          "pathGroups": [
+          distinctGroup: false,
+          groups: ["builtin", "external", "internal", "index"],
+          pathGroups: [
             {
-              "pattern": "react",
-              "group": "external",
-              "position": "before"
+              pattern: "react",
+              group: "external",
+              position: "before",
             },
             {
-              "pattern": "@mui/.*",
-              "group": "external",
+              pattern: "@mui/.*",
+              group: "external",
             },
             {
-              "pattern": "@/**",
-              "group": "internal"
-            }
+              pattern: "@/**",
+              group: "internal",
+            },
           ],
-          "pathGroupsExcludedImportTypes": ["react"],
+          pathGroupsExcludedImportTypes: ["react"],
           "newlines-between": "always",
-          "alphabetize": {
-            "order": "asc",
-            "caseInsensitive": true
-          }
-        }
-      ]
-    }
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+          },
+        },
+      ],
+      "tailwindcss/no-contradicting-classname": "warn",
+      "tailwindcss/enforces-shorthand": "warn",
+      "tailwindcss/no-custom-classname": "off",
+      "tailwindcss/classnames-order": "error",
+    },
   },
 ])
